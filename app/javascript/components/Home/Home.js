f2c76c402218c5c12beaeea20f4d193457c0c28a
RoyNyaga <nyagaandreroy@gmail.com>
2020-11-11 12:49:09 +0100

    add handleVideoChange functionality

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -8,16 +8,24 @@ class Home extends Component {
 
     this.state = {
       course_modules: [
-        { id: 1, title: "1. seting up a new ruby onrails app with react", description: "Dolore ad ullamco adipisicing pariatur.", active: true },
+        { id: 1, title: "1. seting up a new ruby onrails app with react", description: "Dolore ad ullamco adipisicing pariatur.", active: false },
         { id: 2, title: "2. Adding React to an Existing Rails App", description: "Eiusmod anim dolor nostrud culpa eu deserunt irure aliqua et cupidatat ipsum ut irure sunt.", active: false },
         { id: 3, title: "3. Eu ex enim quis nisi.", description: "Laboris adipisicing quis minim anim enim do nulla sunt nisi culpa culpa.", active: false },
-        { id: 4, title: "4. Elit ut duis pariatur et commodo officia deserunt est quis fugiat.", description: "officia deserunt est quis fugiat. Qui officia dolor est ut in deserunt.", active: true }
+        { id: 4, title: "4. Elit ut duis pariatur et commodo officia deserunt est quis fugiat.", description: "officia deserunt est quis fugiat. Qui officia dolor est ut in deserunt.", active: false }
       ]
     }
   }
 
   handleVideoChange(item, event){
     event.preventDefault()
+    let course_modules = [...this.state.course_modules]
+    course_modules.map(data=>{
+      data.active = false
+    })
+    item.active = !item.active
+    course_modules[item.id - 1] = item
+    this.setState({course_modules})
+    console.log(item.active)
   }
 
   render(){
