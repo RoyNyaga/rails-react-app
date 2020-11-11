3cc503ae98a068ca2d26867d1b59b142ce73551a
RoyNyaga <nyagaandreroy@gmail.com>
2020-11-11 13:16:39 +0100

    add seeds to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+course = Course.create(title: "Hello world", description: "create a react app with ruby on rails")
+section = Section.create(title: "Chapter 1", course: course)
+episodes = Episode.create([
+  { title: "1. seting up a new ruby onrails app with react", description: "Dolore ad ullamco adipisicing pariatur.", url: "https://www.youtube.com/embed/s86b3Uu4NWo", section: section },
+  { title: "2. Adding React to an Existing Rails App", description: "Eiusmod anim dolor nostrud culpa eu deserunt irure aliqua et cupidatat ipsum ut irure sunt.", url: "https://www.youtube.com/embed/s86b3Uu4NWo", section: section },
+  { title: "3. Eu ex enim quis nisi.", description: "Laboris adipisicing quis minim anim enim do nulla sunt nisi culpa culpa.", url: "https://www.youtube.com/embed/s86b3Uu4NWo", section: section },
+  { title: "4. Elit ut duis pariatur et commodo officia deserunt est quis fugiat.", description: "officia deserunt est quis fugiat. Qui officia dolor est ut in deserunt.", url: "https://www.youtube.com/embed/s86b3Uu4NWo", section: section }
+])
